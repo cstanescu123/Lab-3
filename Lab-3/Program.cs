404baf3f167fe9494aea43db94588fde5bf75961
cstanescu123 <corneliusemail@gmail.com>
2021-12-05 19:10:19 -0500

    V1 of Lab3

--- Lab-3/Program.cs
+++ Lab-3/Program.cs
@@ -1,2 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hi user! Can you please enter your name?");
+
+string userName = Console.ReadLine();
+
+{
+    string yesOrNo = "n";
+
+    do
+    {
+        {
+            Console.WriteLine($"Hi {userName}! Can you please enter a number between 1 and 100?");
+
+            {
+                int inputNumber = int.Parse(Console.ReadLine());
+
+                int x = inputNumber;
+
+                bool isOdd = x % 2 != 0;
+
+                if (x > 100)
+                    Console.WriteLine($"{userName}, that is not a number between 1 and 100."); 
+                else if (isOdd && x < 60)
+                    Console.WriteLine($"{userName}, {inputNumber} is odd.");
+                else if (!isOdd && x >= 2 && x <= 25)
+                    Console.WriteLine($"{userName}, {inputNumber} is even and less than 25.");
+                else if (!isOdd && x >= 26 && x <= 60)
+                    Console.WriteLine($"{userName}, {inputNumber} is even.");
+                else if (!isOdd && x > 60)
+                    Console.WriteLine($"{userName}, {inputNumber} is even.");
+                else if (isOdd && x > 60)
+                    Console.WriteLine($"{userName}, {inputNumber} is odd.");
+                
+               
+            }
+        }
+        Console.WriteLine("Would you like to try another number? (y/n)");
+        yesOrNo = Console.ReadLine().ToLower();
+    } while (yesOrNo == "y");
+}
+
+
+
+
+
+
+
+
